639bc358c6449c006c98b669645ac96974bcdba9
mohamed-yassine-benkhadda <46766351+mohamed-yassine-benkhadda@users.noreply.github.com>
2020-08-24 15:22:26 +0200

    Add files via upload

--- mastermind.py
+++ mastermind.py
@@ -1,46 +1,78 @@
-#try to test that program in a performer compiler
-#the game requires two players the firstgonna set a code and the other must find it using games hints
 from getpass import getpass
-r=1
-w=4
-p=1
-n=10
-while p==1:
-    if r==2:
-        w=int(input("length of code:"))
-        n=int(input("max attempts:"))
-        r=1
-    if r==3:
-        p=0
-    while r==1:
-        x=getpass("enter code")
-        while len(x)!=w:
-            x=getpass("enter code")
-        for i in range(w):
-            for q in range(w):
-                while x[i]==x[q] and i!=q:
-                    print("do not repeat characters")
-                    x=getpass("enter code")
-        y=input("to start press any button")
-        l=0
-        while x!=y and l<n+1:
-            c=0
-            m=0
-            y=input()
-            if len(y)!=w:
-                print("the code contain",w,"characters")
-            else:
-                l=l+1
-                for i in range(w):
-                    if x[i]==y[i]:
-                        c=c+1
-                    for q in range(w):
-                        if x[i]==y[q] and i!=q:
-                            m=m+1
-                print("correct:",c,"change:",m)
-        if x==y:
-            print("congratulations,attempts:",l)
-        if x!=y:
-            print("hard luck next time")
-            print("the code is :",x)
-        r=int(input("1.play again    2.settings     3.leave"))
+
+def settings():
+    l=int(input("the code contains : "))
+    m=int(input("max attempts : "))
+    return l,m
+
+
+def code(l):
+    print("rules : 1. the code contains",l,"characters      2.Don't repeat characters")
+    x=getpass("enter the code :")
+    if len(x)!=l:
+        return code(l)
+    for i in range(l):
+        for j in range(l):
+            if x[i]==x[j] and i!=j:
+                return code(l)
+    return x
+
+
+
+def answer(l):
+    x=input("enter the code :")
+    if len(x)!=l:
+        return answer(l)
+    for i in range(l):
+        for j in range(l):
+            if x[i]==x[j] and i!=j:
+                return answer(l)
+    return x
+
+
+def exact_place(a,c,l):
+    q=0
+    for i in range(l):
+        if a[i]==c[i]:
+            q+=1
+    return q
+
+
+def change_place(a,c,l):
+    q=0
+    for i in range(l):
+        for j in range(l):
+            if a[i]==c[j] and i!=j:
+                q+=1
+    return q
+
+def menu():
+    q=int(input("1.play again 2.exit"))
+    if q==1:
+        return game()
+
+def game():
+    l,m = settings()
+    c=code(l)
+    a=c[:l-1]+c[0]
+    ex=exact_place(a,c,l)
+    i=0
+    print("guess the secret code using the hints")
+    while ex!=l and i<m:
+        print(m-i," attempts left")
+        a=answer(l)
+        ex=exact_place(a,c,l)
+        ch=change_place(a,c,l)
+        print("correct : ",ex)
+        print("change : ",ch)
+        i+=1
+    if i<m:
+        print("congrats,you win ",i," attempts")
+    else:
+        print("hard luck")
+    return menu()
+    
+game()
+
+
+
